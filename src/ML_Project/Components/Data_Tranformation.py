2bcd963ad27ff0773d6f34e06dfa64972e14ac6d
mohammadhamid8554-lgtm <mohammadhamid8554@gmail.com>
2026-09-13 20:40:22 +0530

    Model Training Completed

--- src/ML_Project/Components/Data_Tranformation.py
+++ src/ML_Project/Components/Data_Tranformation.py
@@ -49,7 +49,7 @@ class DataTransformation:
             return preprocessor
 
         except Exception as e:
-            raise CustomException(e, sys)
+            raise CustomException(e, sys) # type: ignore
 
     def initiate_data_transformation(self, train_path, test_path):
         try:
@@ -100,7 +100,7 @@ class DataTransformation:
 
 
         except Exception as e:
-            raise CustomException(e, sys)
+            raise CustomException(e, sys) # type: ignore
 
     initiate_data_tranformation = initiate_data_transformation
 
